0815b84bd89237f75d66f3b160e7b2a937216e83
Lushanshan336 <51399970+Lushanshan336@users.noreply.github.com>
2019-07-13 21:08:49 +0800

    Update CollectionOperator.java

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,23 +2,37 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        int min = Math.min(left, right);
+        int max = Math.max(left, right);
+        if (left < right) {
+            return IntStream.rangeClosed(min, max).boxed().collect(Collectors.toList());
+        } else {
+            return IntStream.rangeClosed(min, max).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        }
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
+
         throw new NotImplementedException();
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+
+        return Arrays.stream(array).filter(i -> i % 2 == 0).boxed().collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+
+        return Arrays.stream(array).reduce(0, (a, b) -> a = b);
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
@@ -29,3 +43,4 @@ public class CollectionOperator {
         throw new NotImplementedException();
     }
 }
+
